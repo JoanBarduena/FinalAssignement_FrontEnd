13b1bd80189d2b34d96decf9b6c64d624ca43d45
Lidux <36154195+JoanBarduena@users.noreply.github.com>
2022-12-07 18:17:41 -0800

    new lightbox on art work section + gallery imgs redone

---
+++ Final_Assignment/scripts/gallery.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+
+  //Gallery photography
+  let chosen = 0;
+  let page = 0;
+
+  function scrollDiv() {
+    $(".thumb-img").animate({
+      scrollLeft: 800 * page
+    })
+  }
+
+  function showImage() {
+    let chose = $(".lightbox-thumb img").eq(chosen);
+    $('.main-image img').attr({
+      src: chose.attr("src")
+    });
+    chose.addClass("current-img").siblings().removeClass("current-img");
+  }
+
+  $(".lightbox-thumb img").on("click", function () {
+    chosen = $(this).index();
+    showImage();
+  })
+
+  $(".controls .arrow.right").on("click", function () {
+    let max = Math.floor($(".lightbox-thumb img").length / 10);
+    page < max ? page++ : page;
+    scrollDiv();
+  })
+
+  $(".controls .arrow.left").on("click", function () {
+    page > 0 ? page-- : page;
+    scrollDiv();
+  })
+
+  $(".main-image .arrow.right").on("click", function () {
+    let max = Math.floor($(".lightbox-thumb img").length);
+    chosen < max ? chosen++ : chosen;
+    showImage();
+  })
+
+  $(".main-image .arrow.left").on("click", function () {
+    chosen > 0 ? chosen-- : chosen;
+    showImage();
+  })
+})
